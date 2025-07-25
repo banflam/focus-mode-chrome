88fd1b2044dc99e948be6a8fffb391ac800804a1
banflam <rishiagrawal2002@gmail.com>
2025-07-25 19:38:23 -0400

    when the user clicks the extension action, the extension checks if it is a valid URL and the state of the current tab

--- background.js
+++ background.js
@@ -3,3 +3,20 @@ chrome.runtime.onInstalled.addListener(() => {
     text: "OFF",
   });
 });
+
+const extensions = "https://developer.chrome.com/docs/extensions";
+const webstore = "https://developer.chrome.com/docs/webstore";
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+    // Retrieve the action badge to check if the extension is on or off
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+
+    const nextState = prevState === "ON" ? "OFF" : "ON";
+
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+  }
+});
